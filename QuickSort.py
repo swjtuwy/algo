86563e59abbd2a9806173f1dbc924091bcc26b18
tdkihrr <1225807864@qq.com>
2017-09-21 22:50:14 +0800

    update quicksort

--- QuickSort.py
+++ QuickSort.py
@@ -1,16 +1,21 @@
-def quickSort(A, p, r):
-    if p < r:
-        q = partition(A, p, r)
-        quickSort(A, p, q-1)
-        quickSort(A, q+1, r)
+def quickSort(A):
+
+    r = len(A)
+    p = 0
+
+    def _quickSort(A, p, r):
+        if p < r:
+            q = partition(A, p, r)
+            _quickSort(A, p, q-1)
+            _quickSort(A, q+1, r)
 
 
-def partition(A, p, r):
-    x = A[r]
-    i = p - 1
-    for j in range(p, r-1):
-        if A[j] <= x:
-            i += 1
-            A[i], A[j] = A[j], A[i]
-    A[i+1], A[r] = A[r], A[i+1]
-    return i + 1
+    def partition(A, p, r):
+        x = A[r]
+        i = p - 1
+        for j in range(p, r-1):
+            if A[j] <= x:
+                i += 1
+                A[i], A[j] = A[j], A[i]
+        A[i+1], A[r] = A[r], A[i+1]
+        return i + 1
